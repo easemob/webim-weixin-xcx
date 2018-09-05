d4dac2db91ffacf6260f08cf616560dcc37138a6
fanjinhao <fanfan@easemob.com>
2018-09-05 20:33:50 +0800

    添加好友时已经是好友不应该提示消息发送成功

--- src/pages/add_new/add_new.js
+++ src/pages/add_new/add_new.js
@@ -23,7 +23,8 @@ Page({
 	},
 
 	add_friend: function(){
-		if(this.data.friend_name == ""){
+		let me = this;
+		if(me.data.friend_name == ""){
 			wx.showToast({
 				title: "好友添加失败！",
 				duration: 1500
@@ -31,12 +32,41 @@ Page({
 			return;
 		}
 		WebIM.conn.subscribe({
-			to: this.data.friend_name
-			// message: "hello!"
+			to: me.data.friend_name
 		});
-		wx.showToast({
-			title: "消息发送成功！",
-			duration: 1500
-		});
+
+		// 判断当前是否存在该好友
+		let rosters = {
+			success: function(roster){
+				var member = [];
+				for(let i = 0; i < roster.length; i++){
+					if(roster[i].subscription == "both"){
+						member.push(roster[i]);
+					}
+				}
+				if(me.isExistFriend(me.data.friend_name, member)){
+					wx.showToast({
+						title: "已经是你的好友",
+						duration: 1500
+					});
+				}
+				else{
+					wx.showToast({
+						title: "消息发送成功！",
+						duration: 1500
+					});
+				}
+				// console.log(member)
+			}
+		};
+		WebIM.conn.getRoster(rosters);
+	},
+	isExistFriend: function(name, list){
+		for(let index = 0; index < list.length; index++){
+			if(name == list[index].name){
+				return true
+			}
+		}
+		return false
 	}
 });
