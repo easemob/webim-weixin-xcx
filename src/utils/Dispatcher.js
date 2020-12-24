95b7a8d9df18d7782ed39bccec265b0a31598a59
myZhangDong <973849793@qq.com>
2020-12-24 17:54:08 +0800

    fix

--- src/utils/Dispatcher.js
+++ src/utils/Dispatcher.js
@@ -1,40 +1,37 @@
 var dispCbs = [];
 var dispIns = [];
 
-function Dispatcher(){
-	dispIns.push(this);
-	dispCbs.push({});
+function Dispatcher() {
+  dispIns.push(this);
+  dispCbs.push({});
 }
+
 Dispatcher.prototype = {
-	on(type, cb) {
-	    let cbtypes = dispCbs[dispIns.indexOf(this)];
-	    let cbs = cbtypes[type] = cbtypes[type] || [];
-	    if (!~cbs.indexOf(cb)) {
-			let includ = false
-			if(cb.name){
-				cbs.forEach(item => {
-					if(item.name == cb.name){
-						includ = true
-					}
-				})
-			}
-	      !includ && cbs.push(cb);
-	    }
-  	},
-	off(type, cb){
-		let cbtypes = dispCbs[dispIns.indexOf(this)];
-		let cbs = cbtypes[type] = (cbtypes[type] || []);
-		let curTypeCbIdx = cbs.indexOf(cb);
-		if(~curTypeCbIdx){
-			cbs.splice(curTypeCbIdx, 1);
-		}
-	},
-	fire(type, ...args){
-		let cbtypes = dispCbs[dispIns.indexOf(this)];
-		let cbs = cbtypes[type] = (cbtypes[type] || []);
-		for(let i = 0; i < cbs.length; i++){
-			cbs[i].apply(null, args);
-		}
-	}
+  on(type, cb) {
+    let cbtypes = dispCbs[dispIns.indexOf(this)];
+    let cbs = cbtypes[type] = cbtypes[type] || [];
+    if (!~cbs.indexOf(cb)) {
+		cbs.push(cb);
+    }
+  },
+
+  off(type, cb) {
+    let cbtypes = dispCbs[dispIns.indexOf(this)];
+    let cbs = cbtypes[type] = cbtypes[type] || [];
+    let curTypeCbIdx = cbs.indexOf(cb);
+    if (~curTypeCbIdx) {
+      cbs.splice(curTypeCbIdx, 1);
+    }
+  },
+
+  fire(type, ...args) {
+    let cbtypes = dispCbs[dispIns.indexOf(this)];
+    let cbs = cbtypes[type] = cbtypes[type] || [];
+
+    for (let i = 0; i < cbs.length; i++) {
+      cbs[i].apply(null, args);
+    }
+  }
+
 };
 module.exports = Dispatcher;
