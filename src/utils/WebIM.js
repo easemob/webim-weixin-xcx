1969117723145878296753d3ccf637b31d5a26b3
myZhangDong <973849793@qq.com>
2021-04-19 14:48:28 +0800

    fix: 更新3.5.2 SDK, 修复群组通知解析出错

--- src/utils/WebIM.js
+++ src/utils/WebIM.js
@@ -1,7 +1,7 @@
 // import Strophe from "../sdk/libs/strophe";
 //import xmldom from "../sdk/libs/xmldom/dom-parser";
 // import websdk from "../sdk2/src/wxEntry";
-import websdk from "../sdk/wxsdk3.5.1";
+import websdk from "../sdk/wxsdk3.5.2";
 import config from "./WebIMConfig";
 
 console.group = console.group || {};
@@ -234,7 +234,35 @@ WebIM.conn = new WebIM.connection({
 // WebIM.conn.listen({
 //   onOpened: () => dispatch({type: Types.ON_OPEND})
 // })
+Math.uuid = function (len, radix) {
+    var CHARS = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
+    var chars = CHARS, uuid = [], i; 
+    radix = radix || chars.length;
 
+    if (len) {
+        // Compact form
+        for (i = 0; i < len; i++) uuid[i] = chars[0 | Math.random() * radix];
+    } else {
+        // rfc4122, version 4 form
+        var r;
+
+        // rfc4122 requires these characters
+        uuid[8] = uuid[13] = uuid[18] = uuid[23] = '-';
+        uuid[14] = '4';
+
+        // Fill in random data. At i==19 set the high bits of clock sequence
+        // as
+        // per rfc4122, sec. 4.1.5
+        for (i = 0; i < 36; i++) {
+            if (!uuid[i]) {
+                r = 0 | Math.random() * 16;
+                uuid[i] = chars[(i == 19) ? (r & 0x3) | 0x8 : r];
+            }
+        }
+    }
+
+    return uuid.join('');
+};
 // export default WebIM;
 module.exports = {
 	"default": WebIM
